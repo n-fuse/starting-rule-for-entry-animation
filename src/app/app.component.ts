efbfa1430927783ac6df2cbdc6f3cfa70097423c
Bello Babakolo <fleetbeekay@gmail.com>
2024-01-26 09:54:50 +0100

    Add presentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Item } from './models/item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'example-application-with-new-css-entry-transition';
+
+  items: Item[] = [
+    {
+      name: 'Item 1 Name',
+    },
+    {
+      name: 'Item 2 Name',
+    },
+    {
+      name: 'Item 3 Name',
+    },
+    {
+      name: 'Item 4 Name',
+    },
+  ];
 }
